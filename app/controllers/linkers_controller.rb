e5ea0a2a6d986b52bf8774c1b94c875d095cd7d6
Jay Pinho <jaypinho@gmail.com>
2015-11-08 21:10:44 -0500

    Cleanup

--- app/controllers/linkers_controller.rb
+++ app/controllers/linkers_controller.rb
@@ -3,23 +3,7 @@ class LinkersController < ApplicationController
 	include ApplicationHelper
 
 	def index
-
-	    respond_to do |format|
-	      format.html do
-	      end
-	      format.csv do
-	        send_data(@@post, :type => 'text/csv')
-	      end
-    end
-
 	end
-
-	def uploader
-
-	  	@@post = find_http_response(params[:CSVfile].tempfile, params[:search_term])
-	    redirect_to action: "index", :format => :csv
-
-  end
 
 	def ping
 
