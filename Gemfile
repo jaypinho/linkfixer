0a22fc7b79e9605c7fedb243c8f1d21e21889614
Jay Pinho <jaypinho@gmail.com>
2015-12-27 21:51:24 -0500

    Switch to Postgres

--- Gemfile
+++ Gemfile
@@ -8,9 +8,7 @@ group :development, :test do
 	gem 'sqlite3'
 end
 
-group :production do
-	gem 'pg'
-end
+gem 'pg'
 
 group :production do
   gem 'rails_12factor'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151228023949) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "sites", force: true do |t|
     t.string   "name"
     t.string   "url"
