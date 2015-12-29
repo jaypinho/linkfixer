a9e1f20105bfe93425dce1abcb45bf06f3ca37da
Jay Pinho <jaypinho@gmail.com>
2015-12-29 11:26:01 -0500

    Update API with full path

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -1,4 +1,18 @@
 class Snapshot < ActiveRecord::Base
   belongs_to :site
   has_many :headlines
+
+  def file_path
+    ENV['S3_FILE_PREFIX'] + filename
+  end
+
+  def as_json(options={})
+      super(:methods => [:file_path],
+            :include => {
+              :site => {:only => [:name]}
+            },
+            :except => [:filename]
+      )
+    end
+
 end
